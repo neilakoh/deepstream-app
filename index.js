13c453d41653e8d5055637781caa6f3e71780d6d
neilakoh <websiteisfun@gmail.com>
2017-04-15 12:25:17 +0800

    fix host

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import DeepstreamServer from 'deepstream.io';
 const C = DeepstreamServer.constants;
 
 const server = new DeepstreamServer({
-  host: '107.22.247.81',
-  port: '80'
+  host: 'deepstream-app.herokuapp.com',
+  port: 80,
 })
 
 server.start()
