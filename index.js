ea86940399b82ccc9ee20c33a84d60f3252404e7
neilakoh <websiteisfun@gmail.com>
2017-04-15 10:56:33 +0800

    updated the port

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import DeepstreamServer from 'deepstream.io';
 const C = DeepstreamServer.constants;
 
 const server = new DeepstreamServer({
-  host: '192.168.43.195',
-  port: 6020
+  host: 'deepstream-test.herokuapp.com',
+  port: 80
 })
 
 server.start()
