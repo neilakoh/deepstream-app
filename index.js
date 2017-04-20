fa632509ecc71e579f509bcc32fc26e015a53fd2
neilanthonyte <neil@snapzio.com>
2017-04-20 10:33:20 +0800

    fixed port 2

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 import DeepstreamServer from 'deepstream.io';
 import MongoDBStorageConnector from 'deepstream.io-storage-mongodb';
 
-
 const C = DeepstreamServer.constants;
 
 const server = new DeepstreamServer({
   host: '0.0.0.0',
-  port: '${PORT}',
+  port: '$PORT',
   connectionString: 'mongodb://deepstreamapp:neil1250978@ds161410.mlab.com:61410/deepstream-app',
   splitChar: '/'
 })
