9107bef6605a6d079a76db60ef55d8512c50c6da
neilanthonyte <neil@snapzio.com>
2017-04-20 10:13:28 +0800

    fixed deepstream mdb

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import DeepstreamServer from 'deepstream.io';
 const C = DeepstreamServer.constants;
 
 const server = new DeepstreamServer({
-  host: 'deepstream-app.herokuapp.com',
-  port: 80,
+  host: '0.0.0.0',
+  port: '${PORT}',
 })
 
 server.start()
